71ce42a33c6610be0a38853934638f9aa85fc8ae
Jose Manuel <hola@josemanuelsalgado.com>
2025-07-27 12:13:37 -0700

    updated

--- pages/api/send_to_fb.js
+++ pages/api/send_to_fb.js
@@ -6,16 +6,19 @@ export default async function handler(req, res) {
   const pixelId = process.env.FB_PIXEL_ID;
   const accessToken = process.env.FB_ACCESS_TOKEN;
 
+  console.log('Pixel ID:', pixelId);
+  console.log('Access Token:', accessToken ? accessToken.substring(0, 10) + '...' : 'No token');
+
   try {
     const response = await fetch(
-      `https://graph.facebook.com/v18.0/${pixelId}/events?access_token=${accessToken}`,
+      `https://graph.facebook.com/v18.0/${pixelId}/events?access_token=${encodeURIComponent(accessToken)}`,
       {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          data: [req.body], // Facebook expects a "data" array
+          data: [req.body],
         }),
       }
     );
