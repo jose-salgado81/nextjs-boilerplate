eca3b8c2271fcb65f00d1f65bf7bc53c36bf554a
Jose Manuel <hola@josemanuelsalgado.com>
2025-07-27 10:39:54 -0700

    send to fb

--- pages/api/send_to_fb.js
+++ pages/api/send_to_fb.js
@@ -1,1 +1,33 @@
-mkdir fb-capi-function && cd fb-capi-function
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Only POST requests allowed' });
+  }
+
+  const pixelId = process.env.FB_PIXEL_ID;
+  const accessToken = process.env.FB_ACCESS_TOKEN;
+
+  try {
+    const response = await fetch(
+      `https://graph.facebook.com/v18.0/${pixelId}/events?access_token=${accessToken}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          data: [req.body], // Facebook expects a "data" array
+        }),
+      }
+    );
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      return res.status(400).json({ error: 'Facebook API error', details: data });
+    }
+
+    return res.status(200).json({ message: 'Event sent to Facebook', fbResponse: data });
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal Server Error', error: error.message });
+  }
+}
